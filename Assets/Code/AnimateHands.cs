fc476cd83d311f227f885d0e2b9adaec731fa3fc
MR-S4NM1 <miguelitogarcia30@outlook.com>
2025-03-03 14:49:50 -0600

    Animate Hands_03-03-2025

--- Assets/Code/AnimateHands.cs
+++ Assets/Code/AnimateHands.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+[System.Serializable]
+public class AnimateHandsModelOnInput
+{
+    public string animationProperty;
+    public InputActionProperty actionProperty;
+}
 
 public class AnimateHands : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] List<AnimateHandsModelOnInput> animationInputs;
+    [SerializeField] Animator animator;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        foreach (AnimateHandsModelOnInput animationInput in animationInputs){
+            float actionValue = animationInput.actionProperty.action.ReadValue<float>();
+            animator.SetFloat(animationInput.animationProperty, actionValue);
+        }
     }
 }
